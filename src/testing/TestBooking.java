3e6e31facd6ba0337824487a16e183512ce9a089
FACEPrep-ProGrad <61002120+FACEPrep-ProGrad@users.noreply.github.com>
2020-03-11 10:16:09 +0530

    Create TestBooking.java

---
+++ src/testing/TestBooking.java
@@ -0,0 +1,23 @@
+package testing;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import services.Booking;
+
+public class TestBooking {
+	Booking book = new Booking();
+
+	@Test
+	public void testBookingMethod() {
+		assertEquals(6000, book.booking(2, 3000),0);
+		assertEquals(1000, book.booking(1, 1000),0);
+		try {
+			book.booking(0, 5000);
+			book.booking(5, 0);
+			book.booking(0, 0);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
