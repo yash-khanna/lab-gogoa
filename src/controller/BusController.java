b9ff7a6685c855c3b1ae118c0858ddd810f141c3
FACEPrep-ProGrad <61002120+FACEPrep-ProGrad@users.noreply.github.com>
2020-03-11 10:11:00 +0530

    Update BusController.java

--- src/controller/BusController.java
+++ src/controller/BusController.java
@@ -10,10 +10,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-/**
- * Servlet implementation class BusController
- */
 @WebServlet(urlPatterns= {"/bus"})
 public class BusController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
