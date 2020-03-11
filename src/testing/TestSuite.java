4485070d48e4a5824c116ab1e9b4352e8835118c
FACEPrep-ProGrad <61002120+FACEPrep-ProGrad@users.noreply.github.com>
2020-03-11 10:19:02 +0530

    Create TestSuite.java

---
+++ src/testing/TestSuite.java
@@ -0,0 +1,12 @@
+package testing;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestBooking.class, TestBus.class, TestFareCalculator.class, TestFlight.class, TestHotel.class,
+		TestTrain.class })
+public class TestSuite {
+
+}
