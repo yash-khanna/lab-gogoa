0d4a2ae13533c0a341c4a8dcec3eda6c38412cf3
FACEPrep-ProGrad <61002120+FACEPrep-ProGrad@users.noreply.github.com>
2020-03-11 10:16:32 +0530

    Create TestBus.java

---
+++ src/testing/TestBus.java
@@ -0,0 +1,59 @@
+package testing;
+
+import model.Bus;
+import java.time.LocalDate;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestBus {
+	LocalDate ld = LocalDate.of(2020, 02, 03);
+	Bus bus = new Bus(2, 1500, "acsleeper", ld);
+
+	@Test
+	public void testGetNoOfPersonMethod() {
+		bus.setNoOfPersons(5);
+		assertEquals(5, bus.getNoOfPersons());
+		bus.setNoOfPersons(1);
+		assertEquals(1, bus.getNoOfPersons());
+		try {
+			bus.setNoOfPersons(0);
+			bus.setNoOfPersons(-5);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testGetRateMethod() {
+		bus.setRates(1300);
+		assertEquals(1300, bus.getRates());
+		bus.setRates(800);
+		assertEquals(800, bus.getRates());
+		try {
+			bus.setRates(0);
+			bus.setRates(-500);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testGetBusType() {
+		bus.setBusType("ACsleeper");
+		assertEquals("ACsleeper", bus.getBusType());
+		try {
+			bus.setBusType(null);
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testSetDate() {
+		try {
+			bus.setDate(null);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
