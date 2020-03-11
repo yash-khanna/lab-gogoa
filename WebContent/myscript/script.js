3e4cc04a3c54e0716d0a0e9bdbde00d4a93678c4
FACEPrep-ProGrad <61002120+FACEPrep-ProGrad@users.noreply.github.com>
2020-03-11 10:10:18 +0530

    Update script.js

--- WebContent/myscript/script.js
+++ WebContent/myscript/script.js
@@ -1,13 +1,13 @@
 $(function() {
-  // contact form animations
+
   $('#contact').click(function() {
     $('#contactForm').fadeToggle();
   })
   $(document).mouseup(function (e) {
     var container = $("#contactForm");
 
-    if (!container.is(e.target) // if the target of the click isn't the container...
-        && container.has(e.target).length === 0) // ... nor a descendant of the container
+    if (!container.is(e.target) 
+        && container.has(e.target).length === 0)
     {
         container.fadeOut();
     }
@@ -16,15 +16,15 @@ $(function() {
 });
 
 $(function() {
-	  // contact form animations
+
 	  $('#contact1').click(function() {
 	    $('#contactForm1').fadeToggle();
 	  })
 	  $(document).mouseup(function (e) {
 	    var container = $("#contactForm1");
 
-	    if (!container.is(e.target) // if the target of the click isn't the container...
-	        && container.has(e.target).length === 0) // ... nor a descendant of the container
+	    if (!container.is(e.target) 
+	        && container.has(e.target).length === 0) 
 	    {
 	        container.fadeOut();
 	    }
@@ -34,15 +34,15 @@ $(function() {
 
 
 $(function() {
-	  // contact form animations
+
 	  $('#contact2').click(function() {
 	    $('#contactForm2').fadeToggle();
 	  })
 	  $(document).mouseup(function (e) {
 	    var container = $("#contactForm2");
 
-	    if (!container.is(e.target) // if the target of the click isn't the container...
-	        && container.has(e.target).length === 0) // ... nor a descendant of the container
+	    if (!container.is(e.target)
+	        && container.has(e.target).length === 0)
 	    {
 	        container.fadeOut();
 	    }
@@ -51,15 +51,15 @@ $(function() {
 	});
 
 $(function() {
-	  // contact form animations
+	  
 	  $('#contact3').click(function() {
 	    $('#contactForm3').fadeToggle();
 	  })
 	  $(document).mouseup(function (e) {
 	    var container = $("#contactForm3");
 
-	    if (!container.is(e.target) // if the target of the click isn't the container...
-	        && container.has(e.target).length === 0) // ... nor a descendant of the container
+	    if (!container.is(e.target) 
+	        && container.has(e.target).length === 0) 
 	    {
 	        container.fadeOut();
 	    }
